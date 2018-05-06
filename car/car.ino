0971de098f493dac0d95f441a3bd0dd9f3e65a4a
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2018-05-07 00:18:35 +0300

    code refactor, +braking system

--- car/car.ino
+++ car/car.ino
@@ -3,22 +3,36 @@
 
 #include <ServoTimer2.h>
 
-#define BAUDRATE 9600
+#define BAUDRATE 115200
 
+// NRF24 pins
 #define NRF_CE 9
 #define NRF_CS 10
 #define NRF_ADDR 0
 #define NRF_CHAN 0x60
 #define NRF_PAYLOAD_SIZE 32
 
+// L298 and steering servo pins
 #define ENG_PWM_PIN 6
 #define FWD_PIN 3
 #define BCWD_PIN 4
-#define STEARING_SERVO_PIN 5
+#define STEERING_SERVO_PIN 5
 
-#define ZERO 1375
+// Steerings params
+#define ZERO 1325
 #define RIGTH ZERO - 350
 #define LEFT ZERO + 350
+
+// Engine mode control params
+#define FREE 0
+#define FORWARD 1
+#define BACKWARD 2
+#define BRAKE 3
+
+// Commands
+#define CMD_NONE 0
+#define CMD_MOVE 1
+#define CMD_BRAKE 2
 
 
 RF24 radio(NRF_CE, NRF_CS);
@@ -27,15 +41,78 @@ uint8_t pipeNo;
 
 uint8_t payload[NRF_PAYLOAD_SIZE];
 
-ServoTimer2 stearing_servo;  // create servo object to control stearing
+ServoTimer2 steering_servo;  // create servo object to control stearing
 
 int8_t eng_direction;  // 1 - fwd, 2 - bcwrd, 0 - none
 uint8_t eng_pwm;      // pwm for engine
-uint16_t p_stearing_val = 0;
-uint16_t stearing_servo_val;  // stearing servo timing
+uint16_t steering_servo_val;  // stearing servo timing
 
 uint8_t delay_ms = 20;  // delay between commands from controller
 long int recieve_package_time = millis();  // last command recieved time
+
+
+/* 
+ *  Set steering servo position.
+ *  timing - between LEFT and RIGTH
+*/
+void set_steering(uint16_t timing) {
+    static uint16_t p_timing = 0;
+
+    if (abs(timing - p_timing) > 1) {  
+      steering_servo.write(timing);  // sets the servo position according to the scaled value
+    }
+    p_timing = timing;
+}
+
+/*
+ * Set engine state for L298. Control both direction and PWM.
+ * In case FREE and BRAKE direction commands pwm value ignores;
+ * 
+ * Direction - one of FREE | FORWARD | BACKWARD | BRAKE
+ * 
+ * FREE - disconnect engine from circuit
+ * BRAKE - short engine terminals
+ */
+void set_engine(uint8_t eng_direction, uint8_t eng_pwm) {
+   switch (eng_direction) {
+      case FREE:
+        // set both INs to same (low or high) AND enable to low;
+        digitalWrite(FWD_PIN, LOW);
+        digitalWrite(BCWD_PIN, LOW);
+        eng_pwm = 0;
+        break;
+
+      case FORWARD:
+        digitalWrite(FWD_PIN, HIGH);
+        digitalWrite(BCWD_PIN, LOW);
+        break;
+
+      case BACKWARD:
+        digitalWrite(BCWD_PIN, HIGH);
+        digitalWrite(FWD_PIN, LOW);
+        break;
+
+      case BRAKE:
+        // set both INs to same (low or high) AND enable to 1;
+        digitalWrite(BCWD_PIN, LOW);
+        digitalWrite(FWD_PIN, LOW);
+        eng_pwm = 255;
+        break;
+    }
+
+    analogWrite(ENG_PWM_PIN, eng_pwm);  // send PWM signal to engine
+};
+
+/*
+ * Simple greeting to determine car status.
+ */
+void hello() {
+  set_steering(ZERO);
+  delay(100);
+  set_steering(RIGTH);
+  delay(100);
+  set_steering(ZERO);
+};
 
 void setup() {
   Serial.begin(BAUDRATE);
@@ -48,7 +125,7 @@ void setup() {
 
   radio.setPayloadSize(NRF_PAYLOAD_SIZE);  // package size
 
-  radio.openReadingPipe(1, address[NRF_ADDR]);  // addresss
+  radio.openReadingPipe(1, address[NRF_ADDR]);  // address
   radio.setChannel(NRF_CHAN);  // chanel
 
   radio.setPALevel(RF24_PA_MAX);  // RF24_PA_MIN | RF24_PA_LOW | RF24_PA_HIGH | RF24_PA_MAX
@@ -63,13 +140,9 @@ void setup() {
   pinMode(BCWD_PIN, OUTPUT);
 
   // stearing servo
-  stearing_servo.attach(STEARING_SERVO_PIN);
-  stearing_servo.write(ZERO);
-  delay(100);
-  stearing_servo.write(RIGTH);
-  delay(100);
-  stearing_servo.write(ZERO);
-  
+  steering_servo.attach(STEERING_SERVO_PIN);
+
+  hello();  
 }
 
 void loop() {  
@@ -78,60 +151,45 @@ void loop() {
     radio.read(&payload, sizeof(payload));
 
     uint8_t command = payload[0];
+    steering_servo_val = map(payload[1], 0, 255, LEFT, RIGTH);  // map 0 - 255 to servo timing
     delay_ms = payload[7];
 
     switch (command) {
-      // move command
-      case 1:
-        stearing_servo_val = map(payload[1], 0, 255, LEFT, RIGTH);  // map 0 - 255 to servo timing
-        eng_direction = payload[2];  // fixme
+      case CMD_NONE:
+        set_engine(FREE, 0);
+        break;
+
+      case CMD_MOVE:
+        eng_direction = payload[2];
         eng_pwm = payload[3];
+    
+        // engine
+        set_engine(eng_direction, eng_pwm);
+
+        break;
+
+      case CMD_BRAKE:
+        set_engine(BRAKE, 255);
 
         break;
     }
 
-    // strearing
-    if (abs(stearing_servo_val - p_stearing_val) > 1) {  
-       stearing_servo.write(stearing_servo_val);  // sets the servo position according to the scaled value
-    }
-    p_stearing_val = stearing_servo_val;
-
-    // engine
-    switch (eng_direction) {
-      // none
-      case 0:
-        digitalWrite(FWD_PIN, LOW);
-        digitalWrite(BCWD_PIN, LOW);
-        break;
-
-      // forward
-      case 1:
-        digitalWrite(FWD_PIN, HIGH);
-        digitalWrite(BCWD_PIN, LOW);
-        break;
-
-      // backward
-      case 2:
-        digitalWrite(BCWD_PIN, HIGH);
-        digitalWrite(FWD_PIN, LOW);
-        break;
-    }
-
-    analogWrite(ENG_PWM_PIN, eng_pwm);  // send PWM signal to engine
+    // streering
+    set_steering(steering_servo_val);
 
     // send debug info
     char t[100];
-    snprintf(t, 100, "eng_pwm: %i, eng_dir: %i, stear: %i", eng_pwm, eng_direction, stearing_servo_val);
+    snprintf(t, 100, "eng_pwm: %i, eng_dir: %i, stear: %i", eng_pwm, eng_direction, steering_servo_val);
     Serial.println(t);
   }
   
   // in case of lose connection
-  if ((millis() - recieve_package_time) > delay_ms) {
+  if ((millis() - recieve_package_time) > delay_ms * 5) {
     Serial.println("Reset state");
 
-    digitalWrite(FWD_PIN, LOW);
-    digitalWrite(BCWD_PIN, LOW);
-    stearing_servo.write(ZERO);
+    set_engine(BRAKE, 0);  // brake by engine
+    
+    set_steering(ZERO);  // set steering to center point
     
     delay(delay_ms);  // wait some time...
   }
--- controller/controller.ino
+++ controller/controller.ino
@@ -1,40 +1,107 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 
-#define BAUDRATE 9600
+#define BAUDRATE 115200
 
+// NRF24 pins
 #define NRF_CE 9
 #define NRF_CS 10
 #define NRF_ADDR 0
 #define NRF_CHAN 0x60
 #define NRF_PAYLOAD_SIZE 32
 
+// Joystics pins
 #define JOY_ENG_PIN 0
-#define JOY_STEARING_PIN 1
+#define JOY_STEERING_PIN 1
 
-#define JOY_THRESHOLD 1
+#define JOY_FIRST_BUTTON 2
 
+// Engine PWM controls
 #define PWM_LOW_LIMIT 50  // low limit of PWM
-#define PWM_LIMIT 255  // limit for PWM of engine (0-255)
+#define PWM_LIMIT 255     // limit for PWM of engine (0-255)
+
+// Engine mode control params
+#define FREE 0
+#define FORWARD 1
+#define BACKWARD 2
+#define BRAKE 3
+
+// Commands
+#define CMD_NONE 0
+#define CMD_MOVE 1
+#define CMD_BRAKE 2
+
 
 struct JOY {
-  uint16_t y_middle;
-  uint16_t x_middle;
+  uint16_t middle;
   uint16_t zero;
   uint16_t maximum;
 };
 
-JOY joy_eng = {.y_middle = 528, .x_middle=512, .zero = 0, .maximum = 1023};
+JOY joy_eng = {.middle = 528, .zero = 0, .maximum = 1023};
+JOY joy_steering = {.middle = 527, .zero = 0, .maximum = 1023};
 
 RF24 radio(NRF_CE, NRF_CS);
 uint8_t address[][6] = {"1Node","2Node","3Node","4Node","5Node","6Node"};
 
-const uint8_t delay_ms = 1000 / 200;
+const uint8_t delay_ms = 1000 / 200;  // Refresh rate - 200 Hz
 
-uint64_t time_from_last_command = 0;  // ms from last command
+uint64_t time_from_last_command = 0;  // ms since last command
+
+
+struct SteeringResponse {
+  bool engaged = false;
+  uint8_t steering_value = 127;  // 0-255 - angle for steering servo
+};
+
+struct EngineResponse {
+  bool engaged = false;
+  uint8_t eng_direction = FREE;  // engine direction FREE | FORWARD | BACKWARD | BRAKE
+  uint8_t eng_pwm = 0;           // pwm for engine 0-255
+};
+
+/*
+ * Handle of engine control, set engine direction and PWM.
+ */
+EngineResponse handle_engine_control(uint16_t acceleration_read) {
+  EngineResponse r;
+
+  // forward
+  if (acceleration_read > joy_eng.middle) {
+    r.eng_direction = FORWARD;
+    r.eng_pwm = map(acceleration_read, joy_eng.middle, joy_eng.maximum, PWM_LOW_LIMIT, PWM_LIMIT);
+    r.engaged = true;
+  }
+  // backward
+  else if (acceleration_read < joy_eng.middle) {
+    r.eng_direction = BACKWARD;
+    r.eng_pwm = map(acceleration_read, joy_eng.middle, 0, PWM_LOW_LIMIT, PWM_LIMIT);
+    r.engaged = true;
+  }
+
+  return r;
+};
+
+/*
+ * Handle steering control.
+ */
+SteeringResponse handle_steering_control(uint16_t steering_read) {
+  SteeringResponse r;
+  
+  if (steering_read > joy_steering.middle) {
+    r.steering_value = map(steering_read, joy_steering.middle, joy_steering.maximum, 128, 255);
+    r.engaged = true;
+  } 
+  else if (steering_read < joy_steering.middle) {
+    r.steering_value = map(steering_read, joy_steering.middle, joy_steering.zero, 126, 0);
+    r.engaged = true;
+  }
+
+  return r;
+};
 
 void setup() {
-  Serial.begin(BAUDRATE); //открываем порт для связи с ПК
+  Serial.begin(BAUDRATE);
 
   radio.begin();
   radio.setAutoAck(1);
@@ -42,7 +109,7 @@ void setup() {
 
   radio.setPayloadSize(NRF_PAYLOAD_SIZE);  // package size
 
-  radio.openWritingPipe(address[NRF_ADDR]);  // addresss
+  radio.openWritingPipe(address[NRF_ADDR]);  // address
   radio.setChannel(NRF_CHAN);  // chanel
 
   radio.setPALevel(RF24_PA_MAX);  // RF24_PA_MIN | RF24_PA_LOW | RF24_PA_HIGH | RF24_PA_MAX
@@ -52,46 +119,52 @@ void setup() {
   radio.stopListening();  // transmiter mode
   
   pinMode(JOY_ENG_PIN, INPUT);
-  pinMode(JOY_STEARING_PIN, INPUT);
+  pinMode(JOY_STEERING_PIN, INPUT);
+  
+  pinMode(JOY_FIRST_BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
   time_from_last_command = millis();
   
-  int8_t eng_direction = 0;  // 1 - fwd, 2 - bcwrd, 0 - none
-  uint8_t eng_pwm = 0;      // pwm for engine
+  uint8_t command = CMD_NONE;  // command by default
 
-  uint16_t stearing_read = analogRead(JOY_STEARING_PIN);
-  uint8_t stearing_val = map(stearing_read, 0, 1023, 0, 255);
+  // steering_control
+  uint16_t steering_read = analogRead(JOY_STEERING_PIN);
 
-  uint16_t acc_read = analogRead(JOY_ENG_PIN);  // acceleration control
+  // acceleration control
+  uint16_t acc_read = analogRead(JOY_ENG_PIN);
 
-  // forward
-  if (acc_read > (joy_eng.y_middle + JOY_THRESHOLD)) {
-    eng_direction = 1;
-    eng_pwm = map(acc_read, joy_eng.y_middle, joy_eng.maximum, PWM_LOW_LIMIT, PWM_LIMIT);
+  SteeringResponse sr = handle_steering_control(steering_read);
+  EngineResponse er = handle_engine_control(acc_read);
+
+  // temporary brake switch is engine joystic switch
+  if (digitalRead(JOY_FIRST_BUTTON) == LOW) {
+    command = CMD_BRAKE;  // brake has most priority
   }
-  // backward
-  else if (acc_read < (joy_eng.y_middle - JOY_THRESHOLD)) {
-    eng_direction = 2;
-    eng_pwm = map(acc_read, joy_eng.y_middle, 0, PWM_LOW_LIMIT, PWM_LIMIT);
+  // else if controls were engaged car should move 
+  else if (sr.engaged || er.engaged) {
+    command = CMD_MOVE;
   }
 
-  char t[100];
-  snprintf(t, 100, "stear: %i, eng_dir: %i, eng_pwm: %i", stearing_val, eng_direction, eng_pwm);
-  Serial.println(t);
-
-  // payload
+  // build payload
   uint8_t payload[NRF_PAYLOAD_SIZE];
-  payload[0] = 1;  // move command
-  payload[1] = stearing_val;
-  payload[2] = eng_direction;
-  payload[3] = eng_pwm;
+  payload[0] = command;
+  payload[1] = sr.steering_value;
+  payload[2] = er.eng_direction;
+  payload[3] = er.eng_pwm;
   payload[7] = delay_ms;
   
   radio.write(payload, sizeof(payload));
 
+  // send debug info
+  char t[100];
+  snprintf(t, 100, "cmd: %i, steer: %i (%i), eng_dir: %i, eng_pwm: %i", 
+                    command, sr.steering_value, steering_read, er.eng_direction, er.eng_pwm);
+  Serial.println(t);
+
+  // throttle
   while ((millis() - time_from_last_command) < delay_ms) {
-    delay(0);  // sleep 1ms
+    delay(0);  // sleep
   }
 }
